d067348410e94d329158a2f9ea2f56e014f9a842
Sai Sashreek D <saisashreekd@gmail.com>
2025-04-09 10:48:53 +0530

    Created New About Component

---
+++ app/_Components/(Home)/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <>
+      <div className="bg-red-400 w-full h-40 text-center">
+        <div className="p-6 flex flex-col justify-center items-center">
+          <h1 className="text-2xl text-white">About LMS EDU Platform</h1>
+          <p className="p-3 text-center text-lg text-white font-medium">
+            LMS Edu Platform is an organization that offers innovative, effective
+            and research based products and services that promote authentic
+            learning opportunities relevant for 21st century learners.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
