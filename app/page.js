cd3c3f37713df59c548486e7bfc91918843aeef4
Sai Sashreek D <saisashreekd@gmail.com>
2025-04-09 10:48:39 +0530

    Added About Section Content

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import About from "./_Components/(Home)/About";
 import AnimatedSlider from "./_Components/(Home)/AnimatedSlider";
 
 export default function Home() {
   return (
     <>
       <AnimatedSlider />
+      <About />
     </>
   );
 }
