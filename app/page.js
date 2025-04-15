69b4e51c253d34496e3c4e5415e99af123f8f0ef
Sai Sashreek D <saisashreekd@gmail.com>
2025-04-15 13:48:34 +0530

    Updated Upcoming and Explore Courses Sections

--- app/page.js
+++ app/page.js
@@ -1,20 +1,19 @@
 import About from "./_Components/(Home)/About";
 import AnimatedSlider from "./_Components/(Home)/AnimatedSlider";
-import CardHover from "./_Components/(Home)/CardHover";
-import Courses from "./_Components/(Home)/Courses";
 import NewCardHover from "./_Components/(Home)/NewCardHover";
-import CoursesLists from "./_Components/(Home)/CoursesLists";
 import Team from "./_Components/(Home)/Team";
+import Courses from "./_Components/(Home)/Courses";
+import ExploreCourses from "./_Components/(Home)/ExploreCourses";
 
 export default function Home() {
   return (
     <>
       <AnimatedSlider />
       <About />
-      <NewCardHover/>
-      <Courses/>
-      <CoursesLists/>
-      <Team/>
+      <NewCardHover />
+      <Courses />
+      <ExploreCourses />
+      <Team />
     </>
   );
 }
